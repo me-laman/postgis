7c45536c3353a7b96b21c5612e74bc42177a479f
Andrey Leonov <a_leonovv@mail.ru>
2019-01-20 00:35:04 +0300

    test 404

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,3 +4,8 @@ async def test_index(cli, tables_and_data):
     response = await cli.get('/')
     assert response.status == 200
     assert '[]' in await response.text()
+
+
+async def test_404(cli):
+    response = await cli.get('/fake')
+    assert response.status == 404
