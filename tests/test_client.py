836db411d6f2a1da690f276869687686cda7b314
Andrey Leonov <a_leonovv@mail.ru>
2019-01-21 15:51:29 +0300

    add some tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -72,3 +72,43 @@ async def test_add_record_without_class_id(cli, sample_data_fixture):
     assert response.status == 400
     text = await response.text()
     assert text == "You have not specified 'class_id' value"
+
+
+async def test_add_record_without_name(cli, sample_data_fixture):
+    response = await cli.post('/polygon',
+                              json={
+                                    "class_id": 10,
+                                    "props": {"ten": 10, "eleven": 11},
+                                    "geom": "SRID=4326;POLYGON((0 0,5 0,5 5,0 5,0 0))"
+                              },
+                              )
+    assert response.status == 400
+    text = await response.text()
+    assert text == "You have not specified 'name' value"
+
+
+async def test_add_record_without_props(cli, sample_data_fixture):
+    response = await cli.post('/polygon',
+                              json={
+                                    "class_id": 10,
+                                    "name": "added name",
+                                    "geom": "SRID=4326;POLYGON((0 0,5 0,5 5,0 5,0 0))"
+                              },
+                              )
+    assert response.status == 400
+    text = await response.text()
+    assert text == "You have not specified 'props' value"
+
+
+async def test_add_without_geom(cli, sample_data_fixture):
+    response = await cli.post('/polygon',
+                              json={
+                                    "class_id": 10,
+                                    "name": "added name",
+                                    "props": {"ten": 10, "eleven": 11},
+                              },
+                              )
+    assert response.status == 400
+    text = await response.text()
+    assert text == "You have not specified 'geom' value"
+
